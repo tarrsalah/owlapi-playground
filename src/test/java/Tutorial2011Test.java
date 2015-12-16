6ac5756d5a121cb39939d1c84a4f0ca8261fa193
tarrsalah <tarrsalah@gmail.com>
2015-12-16 07:54:28 +0100

    load an ontology from an IRI document.

--- src/test/java/Tutorial2011Test.java
+++ src/test/java/Tutorial2011Test.java
@@ -11,6 +11,8 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -19,15 +21,20 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 public class Tutorial2011Test {
 
     private static final IRI documentIRI = IRI.create("http://127.0.0.1:3001/ontology/books.owl");
+    private static final Logger logger = LoggerFactory.getLogger(Tutorial2011Test.class);
     private final OWLOntologyManager manager;
+    private final OWLOntology booksOntology;
 
-    public Tutorial2011Test() {
+    public Tutorial2011Test() throws OWLOntologyCreationException {
         this.manager = OWLManager.createOWLOntologyManager();
+        this.booksOntology = manager.loadOntologyFromOntologyDocument(Tutorial2011Test.documentIRI);
+
     }
 
     @Test
     public void shoudLoadOntology() throws OWLOntologyCreationException {
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(Tutorial2011Test.documentIRI);
-        assertNotNull(ontology);
+        assertNotNull(this.booksOntology);
+        logger.info("shoudLoadOntology: {}",
+                booksOntology.getOntologyID().toString());
     }
 }
